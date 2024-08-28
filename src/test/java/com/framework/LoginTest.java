0949be31ee1fbf15675043d16cf25f8589276344
Siva <>
2024-08-28 09:27:17 +0530

    framework -2

--- src/test/java/com/framework/LoginTest.java
+++ src/test/java/com/framework/LoginTest.java
@@ -1,14 +1,21 @@
 package com.framework;
 
 import org.testng.Assert;
+import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
-public class LoginTest extends BaseSTT{
+public class LoginTest extends BaseSTT {
+    LoginPage loginPage;
+    ProductsPage productsPage;
+
+    @BeforeClass
+    public void initializePages() {
+        loginPage = new LoginPage(driver);
+        productsPage = new ProductsPage(driver);
+    }
 
     @Test
-    public void verifyLoginSuccess(){
-        LoginPage loginPage = new LoginPage(driver);
-        ProductsPage productsPage = new ProductsPage(driver);
+    public void verifyLoginSuccess() {
         loginPage.login("standard_user", "secret_sauce");
         Assert.assertEquals(productsPage.getNumberOfProducts(), 6);
     }
--- src/test/java/com/framework/ProductsPage.java
+++ src/test/java/com/framework/ProductsPage.java
@@ -2,17 +2,30 @@ package com.framework;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+import java.util.List;
 
 public class ProductsPage {
     WebDriver driver;
+    SeleniumActions seleniumActions;
 
     public ProductsPage(WebDriver driver){
         this.driver = driver;
+        seleniumActions = new SeleniumActions(driver);
+        PageFactory.initElements(driver, this);
     }
 
     By products= By.cssSelector(".inventory_item_name ");
 
+    @FindBy(css = ".inventory_item_name ")
+    private List<WebElement> productsList;
+
     public int getNumberOfProducts(){
-        return driver.findElements(products).size();
+        return seleniumActions.getNumberOfElements(productsList);
     }
+
+
 }
